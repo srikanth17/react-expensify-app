6e0585b01f4c73949b512a6956448367819fa031
Srikanth Dorairaj <srikanth@Srikanths-MBP.connect>
2018-10-15 16:08:23 +0100

    bug fixes & improvvements

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,7 @@ import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-    const formattedExpensesTotal = numeral(expensesTotal).format('$0,0.00');
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
         <div>
             <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
